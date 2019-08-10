c1f58ff15049bea7d3809acdde9634c1b9da04d7
Yu Andy Huang <andypotatohy@gmail.com>
2019-08-10 11:26:42 -0400

    ultimate answer

---
+++ theAnswer2LifeTheUniverseAndEverything.m
@@ -0,0 +1,28 @@
+function theAnswer2LifeTheUniverseAndEverything
+
+disp('Let me think about it...')
+
+pause(2);
+
+disp('(one million years passed...)')
+
+pause(2);
+
+disp('(another one million years passed...)')
+
+pause(4);
+
+disp('(still calculating...)')
+
+pause(4);
+
+disp('(be patient, I''m still working on it...)')
+
+pause(3);
+
+disp('Finally, after 7.5 million years, I got the answer and the answer is:')
+
+fprintf('\n\n%d\n\n',42);
+
+% disp('Thanks for your patience!')
+
